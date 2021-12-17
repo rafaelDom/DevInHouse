3b3b687ed39583face455a79bec21a1676e8d534
rafaelDom <raafita.rad@gmail.com>
2021-12-16 21:23:42 -0300

    Create class User

---
+++ DevInFlix/src/usuarios/Usuario.java
@@ -0,0 +1,30 @@
+package usuarios;
+
+public class Usuario {
+	private String nome;
+	private String dataNascimento;
+	private String endereco;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+	
+
+}
