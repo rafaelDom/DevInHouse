ffa76e5ae2e8031121c720e1a323bf4a40ffc869
rafaelDom <raafita.rad@gmail.com>
2022-02-17 22:01:31 -0300

    Exercicio

---
+++ SENAI-M1S09/src/main/java/GuardaEmpresas.java
@@ -0,0 +1,32 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GuardaEmpresas
+ */
+@WebServlet("/novaEmpresa")
+public class GuardaEmpresas extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		String name = req.getParameter("nome");
+		
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("O NOME DA EMPRESA: " + name);
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O NOME DA EMPRESA: " + name);
+	}
+}
