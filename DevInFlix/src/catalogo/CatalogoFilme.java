14985787343604346a91963f19883fdfd51df937
rafaelDom <raafita.rad@gmail.com>
2022-02-18 23:09:03 -0300

    Exercicio

--- DevInFlix/src/catalogo/CatalogoFilme.java
+++ DevInFlix/src/catalogo/CatalogoFilme.java
@@ -3,10 +3,22 @@ package catalogo;
 import java.util.ArrayList;
 import java.util.List;
 
+import comentario.Comentario;
 import filmes.Filme;
 
 public class CatalogoFilme {
 	List<Filme> catalogoFilmes = new ArrayList<Filme>();
+	private Comentario comentario;
+	private Filme filme;
+	
+	
+
+	public CatalogoFilme(Comentario comentario, Filme filme) {
+		super();
+		this.setComentario(comentario);
+		this.filme = filme;
+		adicionarFilmeCatalogo(this.filme);
+	}
 
 	public List<Filme> getFilmes() {
 		return catalogoFilmes;
@@ -14,6 +26,14 @@ public class CatalogoFilme {
 	
 	public void adicionarFilmeCatalogo(Filme filme) {
 		catalogoFilmes.add(filme);
+	}
+
+	public Comentario getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(Comentario comentario) {
+		this.comentario = comentario;
 	}
 	
 }
---
+++ DevInFlix/src/comentario/Comentario.java
@@ -0,0 +1,30 @@
+package comentario;
+
+import java.time.LocalDate;
+
+public class Comentario {
+	private String nomeUsuario;
+	private String comentario;
+	private LocalDate dataHora;
+	
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public LocalDate getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(LocalDate dataHora) {
+		this.dataHora = dataHora;
+	}
+	
+	
+}
